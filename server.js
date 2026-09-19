0077b0ca039007f38c76167df1550cfb7f843b90
Nikhil Palkar <nikhilpalkar02@gmail.com>
2026-09-19 14:15:24 +0000

    Initial MERN VPS app

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("MERN VPS Demo API is running");
+});
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "API is healthy",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
