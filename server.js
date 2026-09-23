24745989e86757b0455756fa62a4c16b9ade46ca
Nikhil Palkar <nikhilpalkar02@gmail.com>
2026-09-23 16:59:27 +0000

    healthy api update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 app.get("/api/health", (req, res) => {
   res.json({
     success: true,
-    message: "API is healthy",
+    message: "API is testing on development branch",
   });
 });
 app.get("/api/users", (req, res) => {
